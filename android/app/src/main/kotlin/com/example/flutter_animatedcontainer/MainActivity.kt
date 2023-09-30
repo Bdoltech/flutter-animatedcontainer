5f8a1ec66fef6aba3c5bd9c98a7bcde0ddadecf3
Benjamin Agbedeyi <samuelbenjamin652@gmail.com>
2023-09-29 12:38:16 -1200

    anicon

---
+++ android/app/src/main/kotlin/com/example/flutter_animatedcontainer/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_animatedcontainer
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  bool selected = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        setState(() {
+          selected = !selected;
+        });
+      },
+      child: Center(
+        child: AnimatedContainer(
+          width: selected ? 200.0 : 100.0,
+          height: selected ? 100.0 : 200.0,
+          color: selected ? Colors.blueGrey : Colors.white,
+          alignment: selected ? Alignment.center : AlignmentDirectional.topCenter,
+          duration: const Duration(seconds: 2),
+          curve: Curves.fastOutSlowIn,
+          child: const FlutterLogo(size: 75),
+
+
+         
+        ),
+      ),
+    );
+  }
+}
